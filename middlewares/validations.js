d10dade34293dac608a49f1eff3d500f070fa2a3
Mohd Sami <msami9503@gmail.com>
2023-10-29 19:37:01 +0000

    input validation

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -1,9 +1,9 @@
 const { body, validationResult } = require('express-validator');
 
 const validatePostRequest = [
-    body('title').notEmpty().withMessage('firstName can not be empty'),
-    body('author').notEmpty().withMessage('lastName can not be empty'),
-    body('summary').notEmpty().withMessage('gender can not be empty'), 
+    body('title').notEmpty().withMessage('title can not be empty'),
+    body('author').notEmpty().withMessage('author can not be empty'),
+    body('summary').notEmpty().withMessage('summary can not be empty'), 
 
   ];
 
